61ec2ea7b82e1c7eefdc3ebf9652c2845123d1b0
nguyentanphatt <bloodomega97@gmail.com>
2024-09-09 15:17:34 +0700

    link component

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import Footer from '../Components/Footer/Footer'
 
 const Shop = () => {
   return (
@@ -9,6 +12,9 @@ const Shop = () => {
       <Hero/>
       <Popular/>
       <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
+      <Footer/>
     </div>
   )
 }

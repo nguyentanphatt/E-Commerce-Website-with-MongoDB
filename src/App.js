49172d3f8bb64c5e6c4cbcd738a9d9736847b54b
nguyentanphatt <bloodomega97@gmail.com>
2024-09-09 13:34:01 +0700

    update assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar/>
     </div>
   );
 }
 
 export default App;
+//840

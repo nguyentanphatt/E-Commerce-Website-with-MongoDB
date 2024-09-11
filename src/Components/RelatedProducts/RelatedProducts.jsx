11fd7ca273826c5134b744382be68c102b39dc1f
nguyentanphatt <bloodomega97@gmail.com>
2024-09-12 00:21:06 +0700

    add related products

---
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './RelatedProducts.css'
+const RelatedProducts = () => {
+  return (
+    <div className="relateproducts">
+        
+    </div>
+  )
+}
+
+export default RelatedProducts

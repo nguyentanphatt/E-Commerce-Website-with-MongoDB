8ccdf93cc98ac17c3c04b0095bdeaa6753dfa53d
nguyentanphatt <bloodomega97@gmail.com>
2024-09-12 00:20:43 +0700

    link component

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react'
 import {ShopContext} from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrums from '../Components/Breadcrums/Breadcrums'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext)
@@ -11,6 +14,9 @@ const Product = () => {
   return (
     <div>
       <Breadcrums product={product}/>
+      <ProductDisplay product={product}/>
+      <DescriptionBox/>
+      <RelatedProducts />
     </div>
   )
 }

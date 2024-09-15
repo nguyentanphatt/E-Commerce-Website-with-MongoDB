28a168eb1f7b2d268aa7d094ce133fdbfeaf8527
nguyentanphatt <bloodomega97@gmail.com>
2024-09-15 14:18:48 +0700

    add product to mongodb

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -36,6 +36,71 @@ app.post("/upload",upload.single('product'),(req,res)=>{
     })
 })
 
+// schema for creating products
+const Product = mongoose.model("Product", {
+    id:{
+        type: Number,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    avilable:{
+        type:Boolean,
+        default:true,
+    },
+})
+
+app.post('/addproduct',async (req,res)=>{
+    //add id product 
+    let products = await Product.find({})
+    let id;
+    if(products.length > 0){
+        let last_product_array = products.slice(-1)
+        let last_product = last_product_array[0]
+        id = last_product.id+1
+    } else{
+        id=1
+    }
+    //add product by schema
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price
+    });
+    console.log(product);
+    await product.save(); //save to database
+    console.log("Save");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
 app.listen(port,(error)=>{
     if(!error){
         console.log("server running on port "+port);
